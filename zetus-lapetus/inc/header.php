782748779051bf1f1d56ff9173167af594d87ec2
Jesse Wollin <jessewollin@gmail.com>
2017-01-27 21:16:11 -0600

    Add files via upload

---
+++ zetus-lapetus/inc/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://use.fontawesome.com/2a4239e3dd.js"></script>
+	<title><?php ?>Zetus Lapetus</title>
+</head>
+<body>
+	<header data-section-name="header" class="header scroll">
+		<div class="container">
+			<nav class="menu header-menu">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Products</a></li>
+					<li><a href="">Contact</a></li>
+				</ul>
+			</nav><!-- .menu .header-menu -->
+			<div class="scroll-arrow">
+				<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div><!-- .container -->
+	</header><!-- .header -->
