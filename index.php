38acc9a0c4f82688749bd4492f735a7844a9cbaf
jesse <jessewollin@gmail.com>
2017-01-31 16:30:19 -0600

    Grids and Layout

--- index.php
+++ index.php
@@ -30,95 +30,18 @@
 	</section><!-- #section0 .section -->
 	<section id="applications" class="scroll section">
 		<div class="container">
-			<div class="scroll-arrow">
-				<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-				</a>
-			</div><!-- .scroll-arrow -->
-			<header class="content-header">
-				<h2>Applications</h2>
-			</header><!-- .content-header -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Play, travel and explore</h3>
-					<ul>
-						<li>user-uploaded and preferred content</li>
-						<li>outer space, deep sea, foreign countries</li>
-						<li>Google Street View + 360 experiences, and more</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Play, travel and explore</h3>
-					<ul>
-						<li>user-uploaded and preferred content</li>
-						<li>outer space, deep sea, foreign countries</li>
-						<li>Google Street View + 360 experiences, and more</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Entertainment and Gaming</h3>
-					<ul>
-						<li>movies, games and interactive shows</li>
-						<li>play sports live with teams around the world</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Training, Education, and Creativity</h3>
-					<ul>
-						<li>programs designed for quick cognitive learning </li>
-						<li>enjoy feedback and reporting</li>
-						<li>painting, building, digital rendering</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div><!-- .quarter -->
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Mental health and meditation</h3>
-					<ul>
-						<li>manage stress and refocus your mind</li>
-						<li>sink into immersive relaxation</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div><!-- .quarter -->
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Shopping</h3>
-					<ul>
-						<li>virtual store walk-throughs</li>
-						<li>real shopping experience with up-to-date inventory</li>
-						<li>easy, secure checkouts</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
+            <div class="col s12 l6">
+                <h2>Download the App</h2>
+                <h4>Play games or go inside your favorite movies. Travel anywhere, learn, shop or hang out with friends in VR</h4>
+                <a href="" class="btn">Explore Experiences</a>
+            </div>
+            <div class="col s4 l6 off-mobile">
+                <img class="floating-img" src="img/floatingphone.png" alt="">
+                <div class="scroll-arrow">
+                    <a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
+                    </a>
+                </div><!-- .scroll-arrow -->
+            </div>
 		</div><!-- .container -->
 	</section><!-- #section1 .section -->
 	<section id="section3" class="scroll section">
@@ -137,15 +60,17 @@
 			</a>
 		</div>
 		<div class="container">
-			<h2>built for Apple IOS + Android</h2>
-			<h3> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
-			<div class="half">
-				<i class="fa fa-apple device-icon" aria-hidden="true"></i>
-				<a href="#" class="btn">Apple VR Package</a>
-			</div>
-			<div class="half">
-				<i class="fa fa-android device-icon" aria-hidden="true"></i>
-				<a href="#" class="btn">Android VR Package</a>
+			<div class="row">
+				<h2>built for Apple IOS + Android</h2>
+				<h3> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
+				<div class="col s12 m6 l6">
+					<i class="fa fa-apple device-icon" aria-hidden="true"></i>
+					<a href="#" class="btn">Apple VR Package</a>
+				</div>
+				<div class="col s12 m6 l6">
+					<i class="fa fa-android device-icon" aria-hidden="true"></i>
+					<a href="#" class="btn">Android VR Package</a>
+				</div>
 			</div>
 		</div><!-- .container -->
 	</section><!-- #section3 .section .page4 -->
