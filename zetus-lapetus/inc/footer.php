7d6e97745888496067e6f5718ebb5e10cd5ac759
Jesse Wollin <jessewollin@gmail.com>
2017-01-31 13:42:35 -0600

    Mergin v1.2 with master
    Mergin v1.2 with master

--- zetus-lapetus/inc/footer.php
+++ zetus-lapetus/inc/footer.php
@@ -1,34 +1,34 @@
-		<footer class="footer">
-			<section class="scroll section">
+		<footer class="footer scroll" data-section-name="footer">
+			<section class="section">
 				<div class="container">
-					<div class="row">
-						<div class="quarter">
-							<h2>Available at</h2>
-						</div><!-- .quarter -->
-						<div class="quarter">
-							<a href="#">
-								<img src="http://placehold.it/150x150">
-							</a>
-						</div><!-- .quarter -->
-						<div class="quarter">
-							<a href="#">
-								<img src="http://placehold.it/150x150">
-							</a>
-						</div><!-- .quarter -->
-						<div class="quarter">
-							<a href="#">
-								<img src="http://placehold.it/150x150">
-							</a>
-						</div><!-- .quarter -->
-					</div>
-					<div class="scroll-arrow">
-						<a href="#section" class="scroll-to-top"><i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i>
+					<div class="quarter">
+						<h2>Available at</h2>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
 						</a>
-					</div>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
+						</a>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
+						</a>
+					</div><!-- .quarter -->
+				</div>
+				<div class="container">
+					<div class="scroll-arrow">
+						<a href="#" class="scroll-to-top"><i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i>
+						</a>
+					</div><!-- .scroll-arrow -->
 					<div class="row">
 						<div class="half">
 							<div class="half">
-								<nav class="menu social-menu">
+								<div class="menu social-menu">
 									<nav class="menu footer-menu">
 										<ul>
 											<li><a href=""><i class="fa fa-facebook-square fa-3x"></i></a></li>
@@ -36,8 +36,8 @@
 											<li><a href=""><i class="fa fa-twitter-square fa-3x"></i></a></li>
 											<li><a href=""><i class="fa fa-instagram fa-3x"></i></a></li>
 										</ul>
-									</nav>
-								</nav><!-- .menu .footer-menu -->
+									</nav><!-- .menu .footer-menu -->
+								</div><!-- .menu .footer-menu -->
 							</div><!-- .half -->
 							<div class="half">
 								<nav class="menu footer-menu">
@@ -47,7 +47,7 @@
 										<li><a href="">Products</a></li>
 										<li><a href="">Contact</a></li>
 									</ul>
-								</nav>
+								</nav><!-- .menu .footer-menu -->
 							</div><!-- .half -->
 							<h2>built for Apple IOS + Android</h2>
 							<h3> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
@@ -66,10 +66,10 @@
 								<input type="textarea" name="message" placeholder="Message">
 							</form>
 						</div><!-- .half -->
-					</div>
+					</div><!-- .row --->
 				</div><!-- .container -->
-			</section>
-		</footer>
+			</section><!-- .section -->
+		</footer><!-- .footer .scroll -->
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	<script type="text/javascript" src="js/jquery.scrollify.min.js"></script>
 	<script src="js/init.js"></script>
--- zetus-lapetus/js/init.js
+++ zetus-lapetus/js/init.js
@@ -1,8 +1,22 @@
-$(function() {
+$(document).ready(function() {
+    var hamburger = $('#hamburger');
+    var menu = $('.main-menu');
+    menu.hide();
+    document.querySelector( "#hamburger" ).addEventListener( "click", function(e) {
+        e.preventDefault();
+        this.classList.toggle( "is-active" );
+        if ($(window).width() < 480 || $(window).height() < 480) {
+            //small screen, load other JS files
+            menu.slideToggle('swing');
+        }
+        else {
+            menu.fadeToggle('swing');
+        }
+    });
     $.scrollify({
         section:".scroll",
         //sectionName:false,
-        scrollSpeed:1100,
+        scrollSpeed: 1000,
         after:function(i) {}
     });
     $(".scroll-btn").click(function(e) {
@@ -12,5 +26,19 @@ $(function() {
     $(".scroll-to-top").click(function(e) {
         e.preventDefault();
         $.scrollify.move("#header");
+    });
+    $(".scroll-to-footer").click(function(e) {
+        e.preventDefault();
+        $.scrollify.move("#footer");
+    });
+    var movementStrength = 100;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $(".header").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 100;
+        $('.header').css("background-position", newvalueX - 50 + "px " + newvalueY + "px");
     });
 });
