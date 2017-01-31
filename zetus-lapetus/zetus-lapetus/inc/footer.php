629c5b18606173b12b79bd5cc88e421b0e29d138
Jesse Wollin <jessewollin@gmail.com>
2017-01-31 13:41:43 -0600

    Merging v1.2 with Master
    Merging v1.2 with Master

---
+++ zetus-lapetus/zetus-lapetus/inc/footer.php
@@ -0,0 +1,80 @@
+		<footer class="footer scroll" data-section-name="footer">
+			<section class="section">
+				<div class="container">
+					<div class="quarter">
+						<h2>Available at</h2>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
+						</a>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
+						</a>
+					</div><!-- .quarter -->
+					<div class="quarter">
+						<a href="#">
+							<img src="http://placehold.it/150x150">
+						</a>
+					</div><!-- .quarter -->
+				</div>
+				<div class="container">
+					<div class="scroll-arrow">
+						<a href="#" class="scroll-to-top"><i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i>
+						</a>
+					</div><!-- .scroll-arrow -->
+					<div class="row">
+						<div class="half">
+							<div class="half">
+								<div class="menu social-menu">
+									<nav class="menu footer-menu">
+										<ul>
+											<li><a href=""><i class="fa fa-facebook-square fa-3x"></i></a></li>
+											<li><a href=""><i class="fa fa-youtube-play fa-3x"></i></a></li>
+											<li><a href=""><i class="fa fa-twitter-square fa-3x"></i></a></li>
+											<li><a href=""><i class="fa fa-instagram fa-3x"></i></a></li>
+										</ul>
+									</nav><!-- .menu .footer-menu -->
+								</div><!-- .menu .footer-menu -->
+							</div><!-- .half -->
+							<div class="half">
+								<nav class="menu footer-menu">
+									<ul>
+										<li><a href="">Home</a></li>
+										<li><a href="">About</a></li>
+										<li><a href="">Products</a></li>
+										<li><a href="">Contact</a></li>
+									</ul>
+								</nav><!-- .menu .footer-menu -->
+							</div><!-- .half -->
+							<h2>built for Apple IOS + Android</h2>
+							<h3> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
+						</div><!-- .half -->
+						<div class="half">
+							<form>
+								<label for="firstname" class="screen-reader-text">First Name</label>
+								<input type="text" name="firstname" placeholder="First Name"><br>
+								<label for="lastname" class="screen-reader-text">Last Name</label>
+								<input type="text" name="lastname" placeholder="Last Name">
+								<label for="email" class="screen-reader-text">Email</label>
+								<input type="text" name="email" placeholder="Email">
+								<label for="subject" class="screen-reader-text">Subject</label>
+								<input type="text" name="subject" placeholder="Subject">
+								<label for="message" class="screen-reader-text">First Name</label>
+								<input type="textarea" name="message" placeholder="Message">
+							</form>
+						</div><!-- .half -->
+					</div><!-- .row --->
+				</div><!-- .container -->
+			</section><!-- .section -->
+		</footer><!-- .footer .scroll -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script type="text/javascript" src="js/jquery.scrollify.min.js"></script>
+	<script src="js/init.js"></script>
+	<!-- <script type="text/javascript" src="js/scrolloverflow.js"></script>
+	<script type="text/javascript" src="js/jquery.fullPage.js"></script>
+	<script type="text/javascript" src="js/jquery.fullpage.extensions.min.js"></script> -->
+</body>
+</html>
---
+++ zetus-lapetus/zetus-lapetus/inc/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,900" rel="stylesheet">
+	<script src="https://use.fontawesome.com/2a4239e3dd.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<title><?php ?>Zetus Lapetus</title>
+</head>
+<body>
+	<header data-section-name="header" class="header scroll">
+		<div class="section">
+			<div class="container">
+				<div class="logo">
+					<img src="http://placehold.it/200x50" alt="#">
+				</div>
+				<div class="menu-container">
+					<div id="hamburger">
+						<div class="hamburger-box">
+							<div class="hamburger-inner"></div>
+						</div><!-- .hamburger-box -->
+					</div><!-- #hamburger -->
+					<nav class="menu main-menu">
+						<ul>
+							<li class="menu-item"><a href="index.php">Home</a></li>
+							<li class="menu-item"><a href="">About</a></li>
+							<li class="menu-item"><a href="">Products</a></li>
+							<li class="menu-item"><a href="#" class="scroll-to-footer">Contact</a></li>
+						</ul>
+					</nav><!-- .menu .header-menu -->
+				</div><!-- .menu-container -->
+			</div><!-- .container -->
+		</div><!-- full-width-container -->
+
+
