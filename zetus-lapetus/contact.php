f2c71cac0b9013011fd4393b2ad8a1c67a395ff7
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-02-03 17:57:06 -0600

    started contact.php

---
+++ zetus-lapetus/contact.php
@@ -0,0 +1,31 @@
+<?php require_once('inc/header.php'); ?>
+	<div class="">
+		<h1>Contact Us</h1>
+	</div><!-- .container -->
+	</header><!-- .header -->
+	<main class="main" id="contact">
+		<section>
+			<div class="row">
+				<div class="contact-intro col s12 m6 l6">
+					<h2>Contact</h2>
+					<h4>Feel free to contact us with questions or concerns. We are happy to help you.</h4>
+				</div>
+				<div class="deets col s12 m6 l6">
+					<a href="tel:612-252-9007"><i class="fa fa-phone fa-2x" aria-hidden="true"></i><p>&nbsp; 612.252.9007</p></a>
+					<a href="https://www.google.com/maps/place/4534+Colfax+Ave+S,+Minneapolis,+MN+55419/@44.9202487,-93.2943225,17z/data=!3m1!4b1!4m5!3m4!1s0x87f627ae99067121:0xf4c956db4ed43366!8m2!3d44.9202487!4d-93.2921285">
+						<i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+						<p>&nbsp; 4534 Colfax Ave S <br> Minneapolis MN 55551</p>
+					</a>
+					<a href="mailto:zl@zetuslapetus.com"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i><p>&nbsp; zl@zetuslapetus.com</p></a>
+				</div>
+			</div><!-- .container -->
+		</section><!-- #section0 .section -->
+		<section id="email-form" class="section">
+			<div class="">
+				<div>
+
+				</div>
+			</div><!-- .container -->
+		</section><!-- #section2 .section -->
+	</main>
+<?php require_once('inc/footer.php'); ?>
