ba27c7aacc9ac89cf99014ac4b611001493199e9
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-02-04 09:59:11 -0600

    styled contact page

--- zetus-lapetus/contact.php
+++ zetus-lapetus/contact.php
@@ -1,27 +1,40 @@
 <?php require_once('inc/header.php'); ?>
-	<div class="">
-		<h1>Contact Us</h1>
+	<div>
+
 	</div><!-- .container -->
 	</header><!-- .header -->
 	<main class="main" id="contact">
 		<section>
-			<div class="row">
+			<div class="row container">
 				<div class="contact-intro col s12 m6 l6">
+					<h1>Contact Us</h1>
 					<h2>Contact</h2>
 					<p>Feel free to contact us with questions or concerns. We are happy to help you.</p>
 				</div>
-				<div class="deets col s12 m6 l6">
-					<a href="tel:612-252-9007"><i class="fa fa-phone fa-2x" aria-hidden="true"></i><p>&nbsp; 612.252.9007</p></a>
-					<a href="https://www.google.com/maps/place/4534+Colfax+Ave+S,+Minneapolis,+MN+55419/@44.9202487,-93.2943225,17z/data=!3m1!4b1!4m5!3m4!1s0x87f627ae99067121:0xf4c956db4ed43366!8m2!3d44.9202487!4d-93.2921285">
-						<i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
-						<p>&nbsp; 4534 Colfax Ave S <br> Minneapolis MN 55551</p>
-					</a>
-					<a href="mailto:zl@zetuslapetus.com"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i><p>&nbsp; zl@zetuslapetus.com</p></a>
-				</div>
+				<ul class="deets col s12 m6 l6">
+					<li>
+						<a href="tel:612-252-9007">
+							<i class="fa fa-phone fa-2x col s2 m2 l1" aria-hidden="true"></i>
+							<span class="col s10 m10 l11">612.252.9007</span>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.google.com/maps/place/4534+Colfax+Ave+S,+Minneapolis,+MN+55419/@44.9202487,-93.2943225,17z/data=!3m1!4b1!4m5!3m4!1s0x87f627ae99067121:0xf4c956db4ed43366!8m2!3d44.9202487!4d-93.2921285">
+							<i class="fa fa-map-marker fa-2x col s2 m2 l1" aria-hidden="true"></i>
+							<span class="col s10 m10 l11">4534 Colfax Ave S <br> Minneapolis MN 55551</span>
+						</a>
+					</li>
+					<li>
+						<a href="mailto:zl@zetuslapetus.com">
+							<i class="fa fa-envelope fa-2x col s2 m2 l1" aria-hidden="true"></i>
+							<span class="col s10 m10 l11">zl@zetuslapetus.com</span>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</section>
 		<section id="email-form">
-			<div class="row">
+			<div class="row container">
 				<form id="form">
 					<p id="returnmessage"></p>
 					<div class="col s12 m6 l6">
