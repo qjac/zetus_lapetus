be16134c159316616d108a43e33104f2cca1008c
Jesse Wollin <jessewollin@gmail.com>
2017-02-04 16:01:23 -0600

    Index file

--- zetus-lapetus/index.php
+++ zetus-lapetus/index.php
@@ -1,153 +1,59 @@
-<?php require_once('inc/header.php'); ?>
+<?php
+$page = "Home";
+require_once('inc/header.php'); ?>
 	<div class="container">
-		<div class="scroll-arrow">
-			<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-			</a>
-		</div>
-		<h1>a virtual reality headset that pairs with IOS + Android platforms</h1>
+        <div class="content col s12 m7 l12">
+        <h1>a virtual reality headset that pairs with IOS + Android platforms</h1>
+            <img class="headset off-mobile" src="img/headsetempty.png" width="600" alt="">
+        </div><!-- content col s12 m7 l7 -->
 	</div><!-- .container -->
 </header><!-- .header -->
 <main class="main">
 	<section id="core-features"  class="section scroll">
 		<div class="container">
-			<div class="content">
-				<h2>Hello Core Features</h2>
-				<ul>
-					<li>works with any platform/size smart device</li>
-					<li>Bluetooth connective</li>
-					<li>high resolution 5k screen technology</li>
-					<li>phone app to connect VR to user’s phone</li>
-					<li>retina tracking for ultimate live experience</li>
-					<li>wireless molded hand remotes</li>
-					<li>competitive affordable pricing</li>
-				</ul>
+			<div class="content col s12 m7 l6 offset-l6">
+				<h2>Zetus Lapetus</h2>
+                <h4>Redefining digital entertainment</h4>
+                <p class="text-center"><a class="button-big button-big-tertiary" href="#">Learn More</a></p>
 			</div><!-- .content -->
-			<div class="scroll-arrow">
-				<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-				</a>
-			</div><!-- .scroll-arrow -->
 		</div><!-- .container -->
 	</section><!-- #section0 .section -->
 	<section id="applications" class="scroll section">
 		<div class="container">
-			<div class="scroll-arrow">
-				<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-				</a>
-			</div><!-- .scroll-arrow -->
-			<header class="content-header">
-				<h2>Applications</h2>
-			</header><!-- .content-header -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Play, travel and explore</h3>
-					<ul>
-						<li>user-uploaded and preferred content</li>
-						<li>outer space, deep sea, foreign countries</li>
-						<li>Google Street View + 360 experiences, and more</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Play, travel and explore</h3>
-					<ul>
-						<li>user-uploaded and preferred content</li>
-						<li>outer space, deep sea, foreign countries</li>
-						<li>Google Street View + 360 experiences, and more</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Entertainment and Gaming</h3>
-					<ul>
-						<li>movies, games and interactive shows</li>
-						<li>play sports live with teams around the world</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div>
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Training, Education, and Creativity</h3>
-					<ul>
-						<li>programs designed for quick cognitive learning </li>
-						<li>enjoy feedback and reporting</li>
-						<li>painting, building, digital rendering</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div><!-- .quarter -->
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Mental health and meditation</h3>
-					<ul>
-						<li>manage stress and refocus your mind</li>
-						<li>sink into immersive relaxation</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
-			<div class="row">
-				<div class="quarter">
-					<img src="http://placehold.it/270x110" alt="#">
-				</div><!-- .quarter -->
-				<div class="threequarter">
-					<i class="fa fa-chevron-right fa-4x" aria-hidden="true"></i>
-					<h3>Shopping</h3>
-					<ul>
-						<li>virtual store walk-throughs</li>
-						<li>real shopping experience with up-to-date inventory</li>
-						<li>easy, secure checkouts</li>
-					</ul>
-				</div><!-- .threequarter -->
-			</div><!-- .row -->
+            <div class="row">
+                <div class="col s12 m6 l7">
+                    <h2>DOWNLOAD THE APP</h2>
+                    <h4>Play games or go inside your favorite movies. Travel anywhere, learn, shop or hang out with friends in VR</h4>
+                    <p class="text-center"><a class="button-big button-big-tertiary" href="#">Explore Experiences</a></p>
+                </div>
+            </div>
 		</div><!-- .container -->
 	</section><!-- #section1 .section -->
-	<section id="section3" class="scroll section">
-		<div class="container">
-			<div class="scroll-arrow">
-				<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-				</a>
-			</div>
-			<h1>Really big VR glasses container</h1>
-			<img src="http://placehold.it/1200x400">
-		</div><!-- .container -->
+	<section id="vrgoggles" class="scroll section">
+        <video width="100%" height="240" controls>
+            <source src="https://www.youtube.com/watch?v=16JzUGdVrBg" type="video/mp4">
+            <source src="https://www.youtube.com/watch?v=16JzUGdVrBg" type="video/ogg">
+            Your browser does not support the video tag.
+        </video>
 	</section><!-- #section2 .section -->
 	<section id="devices" class="scroll section">
-		<div class="scroll-arrow">
-			<a href="#" class="scroll-btn"><i class="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
-			</a>
-		</div>
 		<div class="container">
-			<h2>built for Apple IOS + Android</h2>
-			<h3> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
-			<div class="half">
-				<i class="fa fa-apple device-icon" aria-hidden="true"></i>
-				<a href="#" class="btn">Apple VR Package</a>
-			</div>
-			<div class="half">
-				<i class="fa fa-android device-icon" aria-hidden="true"></i>
-				<a href="#" class="btn">Android VR Package</a>
-			</div>
+            <div class="row">
+                <div class="col s12">
+                    <h2 class="text-center">built for Apple IOS + Android</h2>
+                    <h3 class="text-center"> shop for the accessories you’ll need to get started at Zetus Lapetus retail shop</h3>
+                </div>
+                <div class="device-container col s12 l6">
+                    <p class="text-center"><i class="fa fa-apple device-icon" aria-hidden="true"></i></p>
+                    <p class="text-center"><a class="button-big button-big-tertiary" href="#">Apple VR Package</a></p>
+                </div>
+                <div class="device-container col s12 l6">
+                    <p class="text-center"><i class="fa fa-android device-icon" aria-hidden="true"></i></p>
+                    <p class="text-center"><a class="button-big button-big-tertiary" href="#">Android VR Package</a></p>
+                </div>
+            </div>
 		</div><!-- .container -->
 	</section><!-- #section3 .section .page4 -->
 </main>
 <?php require_once('inc/footer.php'); ?>
+<!-- <script type="text/javascript" src="js/jquery.scrollify.min.js"></script> -->
